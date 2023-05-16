0b716dc7cff950bcfd5945e39bb090986eda2164
Radek Tomasek <radek.tomasek@gmail.com>
2023-05-16 08:45:52 +0100

    Extended the default configuration by utilizing custom variables to make the styles as lean as possible

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,35 @@ module.exports = {
         desaturatedBlue: 'var(--neutral-desaturated-blue-color)',
         darkBlue: 'var(--neutral-dark-blue-color)',
       },
+      fontSize: {
+        '2xs': ['var(--link-size-small)', {
+          lineHeight: 'var(--link-line-small)',
+          fontWeight: '500'
+        }],
+        'xs': ['var(--text-size-tiny)', {
+          lineHeight: 'var(--text-line-tiny)',
+          fontWeight: '500'
+        }],
+        'sm': ['var(--text-size-small)', {
+          lineHeight: 'var(--text-line-small)',
+          fontWeight: '500'
+        }],
+        'base': ['var(--text-size-normal)', {
+          lineHeight: 'var(--text-line-normal)',
+          fontWeight: '500'
+        }],
+        'lg': ['var(--heading-size-medium)', {
+          lineHeight: 'var(--heading-line-medium)',
+          fontWeight: '900'
+        }],
+        'xl': ['var(--heading-size-large)', {
+          lineHeight: 'var(--heading-line-large)',
+          fontWeight: '900'
+        }]
+      },
+      lineHeight: {
+        '5.5': 'var(--text-line-smaller)'
+      },
       fontFamily: {
         redHatDisplay: ['Red Hat Display', 'sans-serif'],
       },
